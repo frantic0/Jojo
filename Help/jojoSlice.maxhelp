1660bc47ba4cf82d11c1bb07ea4ef02b774c17f3
nicolasdanet <nicolas.danet@free.fr>
2014-02-14 11:43:06 +0100

    ...

---
+++ Help/jojoSlice.maxhelp
@@ -0,0 +1,64 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 5,
+			"minor" : 1,
+			"revision" : 9
+		}
+,
+		"rect" : [ 25.0, 69.0, 335.0, 359.0 ],
+		"bglocked" : 0,
+		"defrect" : [ 25.0, 69.0, 335.0, 359.0 ],
+		"openrect" : [ 0.0, 0.0, 0.0, 0.0 ],
+		"openinpresentation" : 0,
+		"default_fontsize" : 14.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 20.0, 20.0 ],
+		"gridsnaponopen" : 0,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"maxclass" : "button",
+					"patching_rect" : [ 100.0, 120.0, 20.0, 20.0 ],
+					"numinlets" : 1,
+					"id" : "obj-3",
+					"numoutlets" : 1,
+					"outlettype" : [ "bang" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "jojoSlice",
+					"patching_rect" : [ 100.0, 180.0, 63.0, 23.0 ],
+					"numinlets" : 1,
+					"fontname" : "Arial",
+					"id" : "obj-1",
+					"numoutlets" : 0,
+					"fontsize" : 14.0
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"source" : [ "obj-3", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+ ]
+	}
+
+}
---
+++ Projects/jojoSlice/Source/jojoSlice.cpp
@@ -0,0 +1,226 @@
+
+/*
+ *  Copyright (c) 2014, Nicolas Danet, 
+ *  "nicolas.danet@free.fr".
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+/* The happiness of timeshare. */
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include <Carbon/Carbon.h>
+#include <QuickTime/QuickTime.h>
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include "ext.h"
+#include "ext_obex.h"
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include "JuceHeader.h"
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+struct _jojo;
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+class JojoClient : public TimeSliceClient {
+
+public: 
+    explicit JojoClient(_jojo *x) : owner(x) { }
+
+public:
+    int useTimeSlice( );
+
+private:
+    _jojo *owner;
+
+private:
+    JUCE_LEAK_DETECTOR(JojoClient)
+};
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+typedef struct _jojo {
+
+public :
+    _jojo( ) : mClient(new JojoClient(this)), mThread("Jojo"), mLock( ) { }
+    ~_jojo( ) { mThread.stopThread(-1); } 
+
+public:
+    t_object                    ob;
+    ulong                       mError;
+    ScopedPointer<JojoClient>   mClient;    /* Before TimeSliceThread. */
+    TimeSliceThread             mThread;    /* Use a ScopedPointer also? */
+    CriticalSection             mLock;
+    void                        *mClock; 
+    
+    } t_jojo;
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+int JojoClient::useTimeSlice( )
+{ 
+    juce::Random rand; 
+    int k = rand.nextInt(10);
+    
+    if (k) {
+        clock_fdelay(owner->mClock, 0.);    /* Always use a clock from custom thread! */
+        return 100;
+    }
+    
+    return -1;          
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+#ifdef WIN_VERSION
+    #define JOJO_EXPORT __declspec(dllexport)
+#else
+    #define JOJO_EXPORT __attribute__((visibility("default")))
+#endif
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+#define JOJO_GOOD   0UL
+#define JOJO_ERROR  1UL
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include <memory>
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void *jojo_new  (t_symbol *s, long argc, t_atom *argv);
+void jojo_free  (t_jojo *x);
+void jojo_task  (t_jojo *x);
+void jojo_bang  (t_jojo *x);
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static t_class *jojo_class;
+
+JOJO_EXPORT int main(void)
+{   
+    t_class *c = NULL;
+    
+    c = class_new("jojoSlice", (method)jojo_new, (method)jojo_free, sizeof(t_jojo), NULL, A_GIMME, 0);
+    class_addmethod(c, (method)jojo_bang, "bang", 0);
+    class_register(CLASS_BOX, c);
+    jojo_class = c;
+    
+    return 0;
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void *jojo_new(t_symbol *s, long argc, t_atom *argv)
+{
+    t_jojo *x = NULL;
+    
+    if ((x = (t_jojo *)object_alloc(jojo_class))) {
+    //
+    ulong err = (x->mError = JOJO_GOOD);
+    
+    err |= !(x->mClock = clock_new(x, (method)jojo_task));
+    
+    try {
+        new(x)t_jojo;
+        
+        x->mThread.startThread( );     /* All the thread creation machinery is done there. */
+    }
+    
+    catch (...) {
+        err = (x->mError = JOJO_ERROR);
+    }
+    
+    if (err) {
+        object_free(x);
+        x = NULL;
+    }
+    //
+    }
+    
+    return x;
+}
+
+void jojo_free(t_jojo *x)
+{
+    if (!x->mError) { x->~t_jojo( ); }
+    
+    if (x->mClock) {
+        object_free(x->mClock);
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void jojo_task(t_jojo *x)
+{
+    post("Le sentier longeait la falaise.");
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void jojo_bang(t_jojo *x)
+{
+    const ScopedLock myLock(x->mLock);
+    
+    if (!x->mThread.getNumClients( )) {
+        x->mThread.addTimeSliceClient(x->mClient, 0.);
+    } else {
+        post("I'm already working!");
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
