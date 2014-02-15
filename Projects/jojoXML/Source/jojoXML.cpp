342ef92bc7a349e5c10629762d35700f5b197b6e
nicolasdanet <nicolas.danet@free.fr>
2014-02-15 14:54:59 +0100

    ...

---
+++ Projects/jojoXML/Source/jojoXML.cpp
@@ -0,0 +1,211 @@
+
+/*
+ *  Copyright (c) 2014, Nicolas Danet, 
+ *  "nicolas.danet@free.fr".
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+/* XML experiments. */
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include <Carbon/Carbon.h>
+#include <QuickTime/QuickTime.h>
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include "ext.h"
+#include "ext_obex.h"
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include "JuceHeader.h"
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+typedef struct _jojo {
+
+public :
+    _jojo( ) : mLock( ) { }
+
+public:
+    t_object        ob;
+    ulong           mError;
+    CriticalSection mLock;
+    
+    } t_jojo;
+    
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+#ifdef WIN_VERSION
+    #define JOJO_EXPORT __declspec(dllexport)
+#else
+    #define JOJO_EXPORT __attribute__((visibility("default")))
+#endif
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+#define JOJO_GOOD   0UL
+#define JOJO_ERROR  1UL
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include <memory>
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void *jojo_new  (t_symbol *s, long argc, t_atom *argv);
+void jojo_free  (t_jojo *x);
+void jojo_bang  (t_jojo *x);
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+void jojo_write (t_jojo *x, const File& aFile);
+void jojo_read  (t_jojo *x, const File& aFile);
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static t_class *jojo_class;
+
+JOJO_EXPORT int main(void)
+{   
+    t_class *c = NULL;
+    
+    c = class_new("jojoXML", (method)jojo_new, (method)jojo_free, sizeof(t_jojo), NULL, A_GIMME, 0);
+    class_addmethod(c, (method)jojo_bang, "bang", 0);
+    class_register(CLASS_BOX, c);
+    jojo_class = c;
+    
+    return 0;
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void *jojo_new(t_symbol *s, long argc, t_atom *argv)
+{
+    t_jojo *x = NULL;
+    
+    if ((x = (t_jojo *)object_alloc(jojo_class))) {
+    //
+    ulong err = (x->mError = JOJO_GOOD);
+    
+    try {
+        new(x)t_jojo;
+    }
+    
+    catch (...) {
+        err = (x->mError = JOJO_ERROR);
+    }
+
+    if (err) {
+        object_free(x);
+        x = NULL;
+    }
+    //
+    }
+    
+    return x;
+}
+
+void jojo_free(t_jojo *x)
+{
+    if (!x->mError) { x->~t_jojo( ); }
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void jojo_bang(t_jojo *x)
+{
+    const ScopedLock myLock(x->mLock); 
+    
+    File xmlFile((File::getSpecialLocation(File::currentApplicationFile)).getSiblingFile("jojoXML.txt"));
+    
+    jojo_write(x, xmlFile);
+    jojo_read(x, xmlFile);          /* Caution : fetch the attributes is inefficient. */
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void jojo_write(t_jojo *x, const File& aFile)
+{
+    XmlElement myDocument("Jojo");
+    XmlElement* myElement = myDocument.createNewChildElement("Lapin");
+
+    myElement->setAttribute("Carottes", 4);
+    myElement->setAttribute("Oreilles", "Deux");
+    myElement->setAttribute("Rapide", true);
+
+    post("%s", myDocument.createDocument(String::empty, true).toRawUTF8( ));
+    myDocument.writeToFile(aFile, String::empty);
+}
+    
+void jojo_read(t_jojo *x, const File& aFile)
+{
+    XmlDocument myDocument(aFile);
+    XmlElement* myElement = myDocument.getDocumentElement( );
+    
+    if (myElement == nullptr) { post("%s", myDocument.getLastParseError( ).toRawUTF8( )); }
+    else if (myElement->hasTagName("Jojo")) {
+    //
+    forEachXmlChildElement(*myElement, child)
+    {
+    //
+    /* Traverse the linked list once. */
+    
+    const int count = child->getNumAttributes( );   
+    
+    /* Traverse the linked list twice at each iteration! */
+    
+    for (int i = 0; i < count; ++i) {
+        post("%s %s", child->getAttributeName(i).toRawUTF8( ), child->getAttributeValue(i).toRawUTF8( ));
+    }
+    //
+    }
+    //
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
