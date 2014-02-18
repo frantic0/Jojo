32aa708c0ffa227f78f7de6de65ec8df87e856e0
nicolasdanet <nicolas.danet@free.fr>
2014-02-18 11:55:22 +0100

    ...

---
+++ Help/jojoRSA.maxhelp
@@ -0,0 +1,64 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"appversion" : 		{
+			"major" : 5,
+			"minor" : 1,
+			"revision" : 9
+		}
+,
+		"rect" : [ 25.0, 69.0, 288.0, 303.0 ],
+		"bglocked" : 0,
+		"defrect" : [ 25.0, 69.0, 288.0, 303.0 ],
+		"openrect" : [ 0.0, 0.0, 0.0, 0.0 ],
+		"openinpresentation" : 0,
+		"default_fontsize" : 14.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 20.0, 20.0 ],
+		"gridsnaponopen" : 0,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"maxclass" : "button",
+					"numinlets" : 1,
+					"id" : "obj-3",
+					"numoutlets" : 1,
+					"outlettype" : [ "bang" ],
+					"patching_rect" : [ 80.0, 100.0, 20.0, 20.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "jojoRSA",
+					"numinlets" : 1,
+					"id" : "obj-1",
+					"fontname" : "Arial",
+					"numoutlets" : 0,
+					"patching_rect" : [ 80.0, 160.0, 61.0, 23.0 ],
+					"fontsize" : 14.0
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"source" : [ "obj-3", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+ ]
+	}
+
+}
---
+++ Projects/jojoRSA/Source/jojoRSA.cpp
@@ -0,0 +1,181 @@
+
+/*
+ *  Copyright (c) 2014, Nicolas Danet, 
+ *  "nicolas.danet@free.fr".
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+/* RSA and MemoryBlock. */
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include <Carbon/Carbon.h>
+#include <QuickTime/QuickTime.h>
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include "ext.h"
+#include "ext_obex.h"
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include "JuceHeader.h"
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+typedef struct _jojo {
+
+public :
+    _jojo( ) : mPublic( ), mPrivate( ), mLock( ) { }
+
+public:
+    t_object        ob;
+    ulong           mError;
+    RSAKey          mPublic;
+    RSAKey          mPrivate;
+    CriticalSection mLock;
+    
+    } t_jojo;
+    
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+#ifdef WIN_VERSION
+    #define JOJO_EXPORT __declspec(dllexport)
+#else
+    #define JOJO_EXPORT __attribute__((visibility("default")))
+#endif
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+#define JOJO_GOOD   0UL
+#define JOJO_ERROR  1UL
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+
+#include <memory>
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void *jojo_new  (t_symbol *s, long argc, t_atom *argv);
+void jojo_free  (t_jojo *x);
+void jojo_bang  (t_jojo *x);
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+static t_class *jojo_class;
+
+JOJO_EXPORT int main(void)
+{   
+    t_class *c = NULL;
+    
+    c = class_new("jojoRSA", (method)jojo_new, (method)jojo_free, sizeof(t_jojo), NULL, A_GIMME, 0);
+    class_addmethod(c, (method)jojo_bang, "bang", 0);
+    class_register(CLASS_BOX, c);
+    jojo_class = c;
+    
+    return 0;
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void *jojo_new(t_symbol *s, long argc, t_atom *argv)
+{
+    t_jojo *x = NULL;
+    
+    if ((x = (t_jojo *)object_alloc(jojo_class))) {
+    //
+    ulong err = (x->mError = JOJO_GOOD);
+    
+    try {
+        new(x)t_jojo;
+        
+        RSAKey::createKeyPair(x->mPublic, x->mPrivate, 128);
+    }
+    
+    catch (...) {
+        err = (x->mError = JOJO_ERROR);
+    }
+
+    if (err) {
+        object_free(x);
+        x = NULL;
+    }
+    //
+    }
+    
+    return x;
+}
+
+void jojo_free(t_jojo *x)
+{
+    if (!x->mError) { x->~t_jojo( ); }
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+#pragma mark -
+
+void jojo_bang(t_jojo *x)
+{
+    const ScopedLock myLock(x->mLock); 
+    
+    post("Public / %s", x->mPublic.toString( ).toRawUTF8( ));
+    post("Private / %s", x->mPrivate.toString( ).toRawUTF8( ));
+    
+    String myText(CharPointer_UTF8("P\xc3\xa9p\xc3\xa9 p\xc3\xa8te en ao\xc3\xbbt!"));
+    
+    post("%s", myText.toRawUTF8( ));
+    
+    const juce::MemoryBlock blockBegin(myText.toRawUTF8( ), myText.getNumBytesAsUTF8( ) + 1);
+
+    BigInteger crypto;
+    crypto.loadFromMemoryBlock(blockBegin);
+    x->mPublic.applyToValue(crypto);            /* Encrypt. */
+
+    post("%s", crypto.toString(16).toRawUTF8( ));
+    
+    x->mPrivate.applyToValue(crypto);           /* Decrypt. */
+    
+    const juce::MemoryBlock blockEnd(crypto.toMemoryBlock( ));
+    post("%s", blockEnd.toString( ).toRawUTF8( ));
+}
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
